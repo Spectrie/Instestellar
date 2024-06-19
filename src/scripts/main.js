6628ae130dde7baa0e76aa6dde86ae92d34103ee
Spectrie <victor.campbell10@outlook.com>
2024-06-19 18:03:14 -0300

    commit second

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,48 @@
 document.addEventListener('DOMContentLoaded', function(){
     const questions = document.querySelectorAll('[data-tab-question]');
-
     for(let i = 0; i < questions.length; i++){
         questions[i].addEventListener('click',abreouFechaResposta)
     }
 })
+class MobileNavBar{
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+        
+    }
+    animateLinks(){
+        this.navLinks.forEach((link) => {
+            link.style.animation
+            ?(link.style.animation = "")
+            :(link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`)
+        });
+    }
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    init(){
+        if(this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+const mobileNavbar = new MobileNavBar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
+
 
 function abreouFechaResposta(elemento){
     const classe = 'curiosidades__list__item--is-open';
